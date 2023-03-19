0ecbdf4edc1cb4f8222eb215d4c54598cd33933f
Zarinabrandt <zarinabrandt@gmail.com>
2023-03-19 12:27:31 -0700

    live sunday week 6

---
+++ src/week6/Runner.java
@@ -0,0 +1,11 @@
+package week6;
+
+public class Runner {
+    public static void main(String[] args) {
+
+        System.out.println(Prime.isPrime(11));
+
+        System.out.println(Prime.primeInRange(50));
+
+    }
+}
