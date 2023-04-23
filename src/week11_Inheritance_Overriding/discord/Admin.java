70705f28693f63e1e9763c4cffd55ae7a0f9a248
Zarinabrandt <zarinabrandt@gmail.com>
2023-04-23 09:51:06 -0700

    week 11

---
+++ src/week11_Inheritance_Overriding/discord/Admin.java
@@ -0,0 +1,35 @@
+package week11_Inheritance_Overriding.discord;
+
+public class Admin extends DiscordUser{
+
+    public Admin(String name, int id){
+        super("Admin",name,id);
+    }
+
+    public void createChannel(){
+        System.out.println("Creating new discord channel");
+    }
+
+    @Override
+    public void sendMessage() {
+        System.out.println("Sending announcement");
+    }
+
+
+    public void sendMessages(String channel){
+        System.out.println("Sending message to "+ channel);
+    }
+
+}
+/*
+            - Admin class inherits DiscordUser class
+
+        - create constructor to call parent constructor and set up variables (role - Admin, name, id)
+
+        - create method:
+            createChannel()
+                Example output: prints Creating new discord channel
+
+        - override the sendMessage() to print: "Sending announcement"
+
+     */
---
+++ src/week11_Inheritance_Overriding/discord/DiscordUser.java
@@ -0,0 +1,48 @@
+package week11_Inheritance_Overriding.discord;
+
+import java.time.LocalDate;
+
+public class DiscordUser {
+    /*
+       Create an DiscordUser class
+
+       - create variables:
+           role, name, id
+           variable: account creation date
+
+       - create constructor to initialize the variables
+
+       - create a method sendMessage()
+           prints: "Sending message to class chat"
+
+       - create a method joinDiscord(String discordUrl)
+           prints: "Joining $discordUrl"
+
+       - override toString()
+    */
+
+    String role;
+    String name;
+    int id;
+    LocalDate createdDate;
+
+    public DiscordUser(String roles, String name, int id){
+        this.role = role;
+        this.name = name;
+        this.id = id;
+        createdDate = LocalDate.now();
+    }
+
+    public void sendMessage(){
+        System.out.println("Sending message to class chat");
+    }
+
+    public void joinDiscord(String discordUrl){
+        System.out.println("Joining "+ discordUrl);
+    }
+
+    @Override
+    public String toString() {
+        return name + "#" + id + "\nRoles: "+ role + "\nAccount Created: " + createdDate;
+    }
+}
